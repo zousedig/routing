e4809a771459d3f415082e61bdbd1d07b19867c6
zousedig <lorenzo.reyes.educ@ust.edu.ph>
2022-05-04 22:08:57 +0800

    make changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import "./App.css";
+// import Nav from "./Nav";
+// import About from "./About";
+// import Shop from "./Shop";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          {/* <Route path="/about" element={<About />} />
+          <Route path="/shop" element={<Shop />} /> */}
+        </Routes>
+      </Router>
+    </>
   );
 }
-
+const Home = () => {
+  return (
+    <>
+      {console.log("test")}
+      <h1>Homepage</h1>
+    </>
+  );
+};
 export default App;
---
+++ src/Shop.js
@@ -0,0 +1,12 @@
+import react from "react";
+import "./App.css";
+
+function Shop() {
+  return (
+    <div>
+      <h1>Shop Page</h1>
+    </div>
+  );
+}
+
+export default Shop;
